c6c6eacc37187dd05a29e68c494c374c1e832009
apprenant68 <apprenant68@sim-fmu210021.home>
2023-06-22 17:08:11 +0200

    page guide finie

--- UtilisateurViews/GuideView.swift
+++ UtilisateurViews/GuideView.swift
@@ -9,21 +9,18 @@ import SwiftUI
 
 struct GuideView: View {
     var body: some View {
+        
         ZStack{
             Color("Neutre")
             VStack{
-                Text("Guide")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                VStack{
-                    Image("non")
-                        .resizable()
-                        .frame(height: 250)
-                    Text("blablabla")
+//                Text("Guide")
+//                    .font(.largeTitle)
+//                    .fontWeight(.bold)
+                ScrollView{
+                    ForEach(listeDeConseil) { conseils in
+                        SujetView(guideInView: conseils)
+                    }
                 }
-                .background(.white)
-                .cornerRadius(8)
-                .padding()
             }
         }
     }
@@ -34,3 +31,11 @@ struct GuideView_Previews: PreviewProvider {
         GuideView()
     }
 }
+
+var listeDeConseil = [
+    
+    Guide(titre: "N'aie pas honte d'en parler", image: Image("non"), description: "Personne ne mérite d’être harcelé. Le harcèlement peut toucher tous les âges, toutes religions, tous les sexes et toutes les classes sociales. Personne n’est à l’abri mais personne ne devrait avoir à subir ça. Le problème ce n’est pas toi, mais celui qui te harcèle."),
+    Guide(titre: "Ne pas partager, liker ou commenter des contenus inappropriés!", image: Image("publication"), description: "Certaines personnes ont tendance à partager ou commenter des photos ou vidéos pour montrer leur mécontentement et espérer que celles-ci seront supprimées! \rErreur ! Il ne faut surtout pas faire ça pour ne pas donner plus de visibilité et d’ampleur à la situation. En effet, plus un contenu est partagé, liké et commenté et plus il est visible. Ton action, qui partait d’une bonne intention, a  donc un effet inverse et envenime la situation."),
+    Guide(titre: "Signale les contenus problématiques", image: Image("alerte"), description: "Dans la suite du précédent conseil, pour stopper la diffusion d’un contenu inapproprié, l’unique solution est de le signaler et surtout pas de le partager. \r En faisant ça, tu contribues à la protection de la victime contre d’autres éventuels harceleurs !"),
+    
+]
---
+++ UtilisateurViews/SujetView.swift
@@ -0,0 +1,41 @@
+//
+//  SujetView.swift
+//  CyberHelp
+//
+//  Created by apprenant68 on 22/06/2023.
+//
+
+import SwiftUI
+
+struct SujetView: View {
+    var guideInView : Guide
+    
+    var body: some View {
+        
+        VStack{
+                guideInView.image
+                    .resizable()
+                    .frame(height: 250)
+            VStack(alignment: .leading){
+                Text(guideInView.titre)
+                    .foregroundColor(Color("Primaire"))
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding(.bottom)
+                Text(guideInView.description)
+                    .foregroundColor(Color("DeepBlue"))
+            }
+            .padding()
+            }
+            .background(.white)
+            .cornerRadius(8)
+            .padding()
+        }
+}
+
+
+struct SujetView_Previews: PreviewProvider {
+    static var previews: some View {
+        SujetView(guideInView: listeDeConseil[0])
+    }
+}
