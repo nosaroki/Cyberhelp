14a034c390e30ec37c4df58788fce06ece1e37fa
apprenant 23 <apprenant23@MacBook-Pro-de-apprenant-2.local>
2023-06-22 15:42:04 +0200

    add package, chatview

--- ChatDetailsView.swift
+++ ChatDetailsView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct ChatDetailsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       var body: some View {
+        ZStack{
+            Color("Neutre")
+            VStack {
+                
+            }
+        }
     }
 }
 
--- ChatView.swift
+++ ChatView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct ChatView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color("Neutre")
+                .edgesIgnoringSafeArea(.top)
+            VStack{
+                Text("Chat")
+                    .font(.title)
+                    .fontWeight(.bold)
+            
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
