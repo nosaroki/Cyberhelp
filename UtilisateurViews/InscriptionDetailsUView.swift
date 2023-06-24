66803adeab8e6859d5023d43544be039a416245c
MounaMethni GUITHUB <TONEMAILGITHUB>
2023-06-24 16:40:35 +0200

    modification du formulaire Pas FINI

--- UtilisateurViews/InscriptionDetailsUView.swift
+++ UtilisateurViews/InscriptionDetailsUView.swift
@@ -9,47 +9,84 @@ import SwiftUI
 
 struct InscriptionDetailsUView: View {
     
-    @State var photoprofil: UIImage?
-    @State var prénom : String
-    @State var nom : String
-    @State var age : Int
-    @State var body: some View {
-        Color("Neutre")
-            .edgesIgnoringSafeArea(.top)
-        ZStack{
-            ScrollView{
-                VStack{
-                    
-                    HStack{
-                        
-                        Text("Nom")
-                            .font(.title2)
-                            .foregroundColor(.white)
-                            .padding()
-                        TextField("methni", text: $nom)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .padding()
-                        
-                    }
-                    HStack{
-                        Text("Prenom")
-                            .font(.title2)
-                            .foregroundColor(.white)
-                            .padding()
-                        TextField("Mouna", text: $prénom)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .padding()
-                    
-                    
-                }
-            }
-        }
+    //@State var photoprofil: UIImage?
+    @State var prénom : String = "Prénom"
+    @State var nom : String = "Nom"
+    @State var age : String = "20"
+    @State var genre : Genre = .autre
+    @State var telephone : String = ""
+    //@State var adresse : Adresse
+    @State var siret : String = ""
+    @State var email : String = "Email"
+    @State var mdp : String = "Mot de passe"
+    
+   var body: some View {
         
+       ZStack{
+           Color("Neutre").edgesIgnoringSafeArea(.top)
+           ScrollView{
+               VStack{
+                   
+                   formulaireString( textfeild: "Nom", nom: $nom)
+                   formulaireString( textfeild: "Prenom", nom: $prénom)
+                   formulaireString( textfeild: "Email", nom: $email)
+                   formulaireString( textfeild: "Mot de passe", nom: $mdp)
+                   
+                   formulaireInt(textfeild: "Age", age:$age)
+                   formulaireInt(textfeild: "Telephone", age:$telephone)
+                   formulaireInt(textfeild: "Siret", age:$siret)
+               }
+           }
+       }
     }
 }
 
 struct InscriptionDetailsUView_Previews: PreviewProvider {
     static var previews: some View {
         InscriptionDetailsUView()
+    }
+}
+
+struct formulaireString: View {
+    var textfeild : String
+    @Binding var nom : String
+    
+    var body: some View {
+        HStack{
+            
+            Text(textfeild)
+                .font(.title2)
+                .foregroundColor(Color("DeepBlue"))
+                .padding()
+            Spacer()
+            TextField("", text: $nom)
+                .tint(.gray)
+                .frame(width: 200)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+        }
+    }
+}
+struct formulaireInt: View {
+    var textfeild : String
+    @Binding var age : String
+    
+    var body: some View {
+        HStack{
+            
+            Text(textfeild)
+                .font(.title2)
+                .foregroundColor(Color("DeepBlue"))
+                .padding()
+            Spacer()
+            TextField("", text: $age)
+                .keyboardType(.numberPad)
+                .tint(.gray)
+                .frame(width: 200)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+        }
     }
 }
