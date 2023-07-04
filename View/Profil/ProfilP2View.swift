f9f145fde846784a6e3dd9c38532bb7551297ff6
apprenant68 <apprenant68@sim-fmu210021.home>
2023-07-04 16:37:44 +0200

    profilProView updated

--- View/Profil/ProfilP2View.swift
+++ View/Profil/ProfilP2View.swift
@@ -13,84 +13,82 @@ struct ProfilP2View: View {
     @State private var selectedTopicProfil : TopicOptionProfil = .profil
     
     var body: some View {
-        
-        ZStack{
-            Color("Neutre")
-            
-            VStack{
-                Picker((""), selection: $selectedTopicProfil) {
-                    ForEach(TopicOptionProfil.allCases, id:\ .self) { topic in
-                        Text(topic.rawValue)
-                    }
-                }
-                .pickerStyle(.segmented)
-                .padding()
-                .foregroundColor(Color("DeepBlue"))
-                ScrollView{
-                    if let profile = profileVM.monProfil{
-                        HStack(alignment: .top){
-                            Image(uiImage: UIImage(data: profile.profilPic  ?? Data()) ?? UIImage())
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 170, height: 170)
-                                .cornerRadius(16)
-                                .clipped()
-                                .padding(.top)
-                                .padding(.horizontal)
-                        
-                            VStack(alignment: .leading){
-                                Text("\(profile.nom!) \(profile.prenom!)")
-                                    .font(.title2)
-                                    .lineLimit(1)
-                                    .scaledToFit()
-                                    .fontWeight(.bold)
-                                    .foregroundColor(Color("DeepBlue"))
-                                Text("Psychologue")
-                                    .padding(.bottom, 2)
-                                    .foregroundColor(Color("DeepBlue"))
-                                    .opacity(0.5)
+
+            ZStack{
+                Color("Neutre")
+                    .edgesIgnoringSafeArea(.top)
+                VStack{
+                    ScrollView{
+                        if let profile = profileVM.monProfil{
+                            HStack(alignment: .top){
+                                Image(uiImage: UIImage(data: profile.profilPic  ?? Data()) ?? UIImage())
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 120, height: 120)
+                                    .clipShape(Circle())
+                                    .overlay(
+                                        Circle()
+                                            .stroke((Color("DeepBlue")), lineWidth: 2)
+                                            .frame(width: 122, height: 122)
+                                    )
+                                    .padding(.top)
+                                    .padding(.horizontal)
                                 
-                                HStack{
-                                    Image(systemName: "mappin.circle")
-                                        .font(.system(size: 22))
-                                        .padding(.trailing, 1)
-                                    Text(profile.adresse!)
+                                VStack(alignment: .leading){
+                                    Text("\(profile.nom!) \(profile.prenom!)")
+                                        .font(.title)
+                                        .lineLimit(1)
+                                        .scaledToFit()
+                                        .fontWeight(.bold)
                                         .foregroundColor(Color("DeepBlue"))
-                                        .opacity(0.5)
-                                }
-//                                .padding(.bottom)
-                                HStack(alignment: .top){
-                                    Image(systemName: "graduationcap")
-                                    VStack(alignment: .leading){
-                                        Text("Experience")
+                                    Text("Psychologue")
+                                        .padding(.bottom, 2)
+                                        .foregroundColor(Color("DeepBlue"))
+                                        .opacity(0.7)
+                                    
+                                    HStack{
+                                        Image(systemName: "mappin.circle")
                                             .foregroundColor(Color("DeepBlue"))
-                                            .opacity(0.5)
-                                        Text("6 ans")
+                                            .padding(.trailing, 1)
+                                        Text(profile.adresse!)
+                                            .font(.system(size: 16))
                                             .foregroundColor(Color("DeepBlue"))
                                     }
-                                    .padding(.top, 4)
+                                    //                                .padding(.bottom)
+                                    HStack(){
+                                        Image(systemName: "graduationcap")
+                                            .foregroundColor(Color("DeepBlue"))
+                                            .font(.system(size: 14))
+                                        Text("6 ans d'expérience")
+                                            .padding(.horizontal, -2)
+                                            .font(.system(size: 16))
+                                            .foregroundColor(Color("DeepBlue"))
+                                    }
+                                    .padding(.top, -8)
+                                    .padding(.bottom, 16)
+                                    
                                 }
+                                .padding(.vertical)
+                                Spacer()
                             }
-                            .padding(.vertical)
-                            Spacer()
+                            VStack(alignment:.leading){
+                                Text("Biographie")
+                                    .foregroundColor(Color("DeepBlue"))
+                                    .fontWeight(.bold)
+                                    .font(.title2)
+                                Text("Je suis psychologue cognitiviste-comportementaliste, je reçois en mon cabinet situé à Vaux-le-Penil, en Seine et Marne, les enfants adolescents et adultes, en demande d'intervention psychologique efficace et mesurable. \r\rJe propose une thérapie comportementale fondée sur l'extinction du comportement inadapté et le renforcement des comportements appropriés, par apprentissage systématique, adapté et généralisable.")
+                                    .padding(.top,-4)
+                                    .foregroundColor(Color("DeepBlue"))
+                                    .opacity(0.5)
+                            }
+                            .padding(4)
+                            .padding()
                         }
-                        VStack(alignment:.leading){
-                            Text("Biographie")
-                                .foregroundColor(Color("DeepBlue"))
-                                .fontWeight(.bold)
-                            Text("Je suis psychologue cognitiviste-comportementaliste, je reçois en mon cabinet situé à Vaux-le-Penil, en Seine et Marne, les enfants adolescents et adultes, en demande d'intervention psychologique efficace et mesurable. Je propose une thérapie comportementale fondée sur l'extinction du comportement inadapté et le renforcement des comportements appropriés, par apprentissage systématique, adapté et généralisable. En parallèle, je propose une thérapie cognitive, fondée sur la restructuration du traitement de l'information, l'apprentissage de la souplesse cognitive et la remédiation, afin d'obtenir une perception pertinente de la réalité et un comportement maîtrisé.")
-                                .padding(.top,2)
-                                .foregroundColor(Color("DeepBlue"))
-                                .opacity(0.5)
-                        }
-                        .padding(4)
-                        .padding()
                     }
                 }
             }
         }
     }
-}
 
 struct ProfilP2View_Previews: PreviewProvider {
     static var previews: some View {
