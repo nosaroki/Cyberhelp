671772e5193511f1045309e645c9a77fb832c444
karineaiki <ruiz.karine@gmail.com>
2023-07-05 10:27:08 +0200

    some new stuffs

--- Model/StructureChat.swift
+++ Model/StructureChat.swift
@@ -7,10 +7,15 @@
 
 import Foundation
 import SwiftUI
+import CoreData
 
 
 
-struct Conversation : Identifiable {
+struct Conversation : Identifiable, Equatable {
+    static func == (lhs: Conversation, rhs: Conversation) -> Bool {
+        lhs.id == rhs.id
+    }
+    
     var id = UUID()
     var user : Profil
     var conversation : [Message]
@@ -36,6 +41,10 @@ struct ScrollViewOffsetKey: PreferenceKey {
 var profilU = Profil(profilPic: Image("userPhoto"), prénom: "Isabelle", nom: "Pinchon", age: 19, genre: Genre.femme, telephone: 0606060606, adresse: Adresse(codePostal: "3 rue de la Liberté", nomCommune: "75015 Paris"), email: "isabelle@pinchon.fr", mdp: "Bellegossedu93/", isPro: false)
 
 var profilP = Profil(profilPic: Image("ProPhoto"), prénom: "Jasmine", nom: "Lebeau", age: 42, genre: Genre.femme, telephone: 0122334455, adresse: Adresse(codePostal: "99 rue de Charonne", nomCommune: "75011 Paris"), siret: 57773082900197, email: "jasmine@gmail.com", mdp: "J4imel3sfra!ses", isPro: true)
+
+var profil2P = Profil(profilPic: Image("ProPhoto"), prénom: "Marie-Charlotte", nom: "Dubois", age: 54, genre: Genre.femme, telephone: 0102010201, adresse: Adresse(codePostal: "3 rue de la Vilette", nomCommune: "Paris"), email: "mariecharlotte@gmail.com", mdp: "A3edkenc", isPro: true)
+
+
 
 
 
